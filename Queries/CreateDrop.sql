39b61a2bd75eda19873d9ec547f4a9eafd632742
Bogdan Silvasan <bogdan.silve@gmail.com>
2021-10-15 12:23:37 +0300

    SQL Query

--- Queries/CreateDrop.sql
+++ Queries/CreateDrop.sql
@@ -1,12 +1,12 @@
 CREATE TABLE Order_Info (
-	OrderId int PRIMARY KEY NOT NULL,
+	OrderId varchar(255) PRIMARY KEY NOT NULL,
 	PizzaType varchar(255) NOT NULL,
-	FirstName varchar(255),
-	LastName varchar(255),
+	FirstName varchar(255) NOT NULL,
+	LastName varchar(255) NOT NULL,
 	Phone varchar(16) NOT NULL,
 	Email varchar(255) NOT NULL,
-	Adress varchar(255) NOT NULL,
-	Options varchar(255) NOT NULL
+	DeliveryAddress varchar(255) NOT NULL,
+	Options varchar(255)
 )
 GO
 
